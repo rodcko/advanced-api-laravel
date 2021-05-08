a4c696cf72b1d2383e7bb86046f907d17b0d051c
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-08 10:49:34 -0600

    Terminal commands in Laravel

---
+++ app/Console/Commands/SendNewsletterCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+use App\Notifications\NewsletterNotification;
+use App\User;
+use Illuminate\Console\Command;
+
+class SendNewsletterCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:newsletter {emails?*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Envia un correo electronico';
+
+    public function handle()
+    {
+        $emails = $this->argument('emails');
+
+        $builder = User::query();
+
+        if ($emails) {
+            $builder->whereIn('email', $emails);
+        }
+
+        $count = $builder->count();
+
+        if($count) {
+            $this->output->progressStart($count);
+
+            $builder->whereNotNull('email_verified_at')
+                ->each(function (User $user) {
+                    $user->notify(new NewsletterNotification());
+                    $this->output->progressAdvance();
+                })
+                ;
+
+                $this->info("Se enviaron {$count} correos");
+                $this->output->progressFinish();
+        }
+
+        $this->info('No se envio ningun correo');
+    }
+}
