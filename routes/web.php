c52867ac06eb886c0edaa61fddd010af6a2cf455
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-05-08 17:01:00 -0600

    Queues and jobs

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
